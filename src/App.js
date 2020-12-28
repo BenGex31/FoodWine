94233f10152df9dd93a9305f36baa8c7889e2e20
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-12-28 20:21:21 +0100

    Router set up
    Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import {Navbar} from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar dark expand="xxl">
+          <h1 className='text-center'><Link className="navbar-brand text-white" to='/'>FoodWine</Link></h1>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse text-center" id="navbarNavAltMarkup">
+            <div className="navbar-nav text-white">
+            <Link className='nav-item nav-link text-white' to='/Entrées'>Entrées</Link>
+            <Link className='nav-item nav-link text-white' to='/Plats'>Plats</Link>
+            <Link className='nav-item nav-link text-white' to='/Desserts'>Desserts</Link>
+            </div>
+          </div>
+        </Navbar>
+        <Switch>
+          <Route exact path='/'>
+            
+          </Route>
+          <Route path='/Entrées'>
+
+          </Route>
+          <Route path='/Plats'>
+
+          </Route>
+          <Route path='/Desserts'>
+
+          </Route>
+        </Switch>
       </header>
-    </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'jquery/dist/jquery.slim.min.js'
+import 'popper.js/dist/popper.min.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
