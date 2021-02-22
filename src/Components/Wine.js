aba3bea8c42f323d6b5745ad67670422bba89d37
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-02-22 10:34:50 +0100

    Update Wine.js

--- src/Components/Wine.js
+++ src/Components/Wine.js
@@ -4,28 +4,28 @@ import './Wine.css'
 
 const Wine = ({ id, nameWine, year, price, imgWine, description, story, rating}) => {
     return (
-        <Row className="align-items-center blocListWine">
+        <Row className="blocListWine">
             <Col lg="3">
                 <img className="imgListWine" src={imgWine} alt={nameWine + year}></img>
             </Col>
-            <Col className="mt-2 mb-2" lg="9">
+            <Col className="pt-4" lg="9">
                 <h6 className="nameWine">{nameWine + " " + year} {description}</h6>
-                <p className="rating">Note : {rating} / 5</p>
-                <p className="price"> {price} €</p>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target={"#wine"+id}>
+                <button className="rating">Note : {rating.toFixed(1).replace(".",",")} / 5</button>
+                <p className="price">{ price.toFixed(2).replace(".",",") } €</p>
+                <button className="more-infos" type="button" data-toggle="modal" data-target={"#wine"+id}>
                 Plus d'infos
                 </button>
                 <div class="modal fade" id={"wine"+id} tabindex="-1" role="dialog" aria-labelledby={"ModalLabel"+id} aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id={"ModalLabel"+id} > {nameWine} </h5>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id={"ModalLabel"+id} > {nameWine} </h5>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             {story}
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                            <button type="button" className="btn" data-dismiss="modal">Fermer</button>
                         </div>
                         </div>
                     </div>
