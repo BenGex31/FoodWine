21992a1071026a8232c783d6e38906df18848e39
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-03-03 10:06:22 +0100

    Update ListWine.js

--- src/Components/ListWine.js
+++ src/Components/ListWine.js
@@ -4,20 +4,20 @@ import data from '../data.json'
 
 const ListWine = (props) => {
     const choiceCategory = data.filter(choice => choice.category === props.category);
-    //console.log(choiceCategory)
+    
     return (
 
         choiceCategory.filter(choice => choice.price === props.price).map(wine => (
             <Wine
-            id={wine.id}
-            type={wine.type}
-            nameWine={wine.nameWine}
-            description={wine.description}
-            rating={wine.rating}
-            story={wine.story}
-            src={wine.imgWine}
-            price={wine.price}
-            year={wine.year}
+                id={wine.id}
+                type={wine.type}
+                nameWine={wine.nameWine}
+                description={wine.description}
+                rating={wine.rating}
+                story={wine.story}
+                src={wine.imgWine}
+                price={wine.price}
+                year={wine.year}
             />
         ))
     )
