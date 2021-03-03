6308a9dc49586236ed0d08485afffd7a7ce645ab
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2021-03-03 10:06:37 +0100

    Update DishCard.js

--- src/Components/DishCard.js
+++ src/Components/DishCard.js
@@ -10,7 +10,7 @@ import {Link} from 'react-router-dom'
 
 const DishCard = (props) => {
     return (
-        <Card>
+        <Card className="animate__animated animate__fadeIn animate__slower">
             <CardBody className='text-center d-flex align-items-center justify-content-center'>
                 <CardTitle tag='h5'><Link className='nav-item nav-link text-white' to={props.link} >{props.title}</Link></CardTitle>
             </CardBody>
